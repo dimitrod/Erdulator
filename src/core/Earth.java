2e0ef772d0d4d86c162cd9576429f28474bc5243
lenseB <lenseB>
2023-09-11 11:55:29 +0200

    paar Klassen und README

---
+++ src/core/Earth.java
@@ -0,0 +1,69 @@
+package src.core;
+
+public class Earth {
+    int budget;
+    float c02e;
+    int afforestation;
+    int waterLevel;
+    int population;
+    float temperature;
+    int ozoneLayer;
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    public float getC02e() {
+        return c02e;
+    }
+
+    public void setC02e(float c02e) {
+        this.c02e = c02e;
+    }
+
+    public int getAfforestation() {
+        return afforestation;
+    }
+
+    public void setAfforestation(int afforestation) {
+        this.afforestation = afforestation;
+    }
+
+    public int getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(int waterLevel) {
+        this.waterLevel = waterLevel;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getOzoneLayer() {
+        return ozoneLayer;
+    }
+
+    public void setOzoneLayer(int ozoneLayer) {
+        this.ozoneLayer = ozoneLayer;
+    }
+
+
+}
