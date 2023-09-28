3f63a0feef415536d3503f6657d17c3ab3bef648
Vin1X <vincent.walura@hotmail.com>
2023-09-28 22:12:32 +0200

    changed var to let, deleted function calls

--- src/js/init.js
+++ src/js/init.js
@@ -1,24 +1,21 @@
-var year = 1900
-var budget = 100  * Math.pow(10,6)
-var co2e = 0.02
-var afforestation = 30
-var waterLevel = 0
-var population = 1650000000
-var animalSpecies = 5000000
-var temperature = 0
-var ozoneLayer = 100
+let year = 1900
+let budget = 100  * Math.pow(10,6)
+let co2e = 0.02
+let afforestation = 30
+let waterLevel = 0
+let population = 1650000000
+let animalSpecies = 5000000
+let temperature = 0
+let ozoneLayer = 100
 
-var yearElem = document.getElementById("year")
-var yearElemPercent = document.getElementById("yearpercent")
+let yearElem = document.getElementById("year")
+let yearElemPercent = document.getElementById("yearpercent")
 
-var budgetElem = document.getElementById("budget")
-var co2eElem = document.getElementById("co2e")
-var afforestationElem = document.getElementById("afforestation")
-var waterLevelElem = document.getElementById("waterLevel")
-var populationElem = document.getElementById("population")
-var animalSpeciesElem = document.getElementById("animalSpecies")
-var temperatureElem = document.getElementById("temperature")
-var ozoneLayerElem = document.getElementById("ozoneLayer")
-
-updateAttributes()
-timeIncrement()
+let budgetElem = document.getElementById("budget")
+let co2eElem = document.getElementById("co2e")
+let afforestationElem = document.getElementById("afforestation")
+let waterLevelElem = document.getElementById("waterLevel")
+let populationElem = document.getElementById("population")
+let animalSpeciesElem = document.getElementById("animalSpecies")
+let temperatureElem = document.getElementById("temperature")
+let ozoneLayerElem = document.getElementById("ozoneLayer")
