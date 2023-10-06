63df71043422e4026ecb97e35c7b64a226394acb
Vin1X <vincent.walura@hotmail.com>
2023-10-06 12:15:26 +0200

    added config to change amount of quizzes

--- src/js/quizHandler.js
+++ src/js/quizHandler.js
@@ -1,14 +1,21 @@
 let currentQuestions = []
+let amount
 const answers = [document.getElementById("answer0"), document.getElementById("answer1"), document.getElementById("answer2")]
 const rewardElem = document.getElementById("budgetGrowth")
+
+function quizConfig(quizAmount) {
+    amount = quizAmount
+    console.log(amount)
+}
 
 function makeQuiz(){
     if (quiz.length == 0) {
         document.getElementById("emptyQuiz").style.display="block"
     }
     else {
-        let amountOfQuestions = Math.floor(Math.random() * 3 +1)
-        while (quiz.length !== 0 && amountOfQuestions !== 0){
+        if (!amount) amountOfQuestions = Math.floor(Math.random() * 3 +1)
+        else amountOfQuestions = amount        
+    while (quiz.length !== 0 && amountOfQuestions !== 0){
             let randomNumber =Math.floor(Math.random()*quiz.length)
             let question = quiz.splice(randomNumber, 1)
             currentQuestions.push(question[0]) // slice removed die Frage und gibt ein array mit den removeten Fragen zurück
