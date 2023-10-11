28b2cb95dd6d004318d22072f8c6bbfa2bd0f4af
lenseB <lenseB>
2023-10-11 20:07:44 +0200

    windows button and window with blur effect

---
+++ src/js/upgradeWindow.js
@@ -0,0 +1,18 @@
+upgradeWindow = document.getElementById("upgradeWindow")
+gamePage = document.getElementById("gamePage")
+starWrapper = document.getElementById("starWrapper")
+
+function openUpgradeWindow(){
+    upgradeWindow.style.display= "block"
+    gamePage.style.filter = "blur(5px)"
+    starWrapper.style.filter = "blur(5px)"
+    gamePage.style.userSelect = "none"
+    gamePage.style.pointerEvents = "none"
+}
+function closeUpgradeWindow(){
+    upgradeWindow.style.display= "none"
+    gamePage.style.filter = "none"
+    starWrapper.style.filter = "none"
+    gamePage.style.userSelect = "all"
+    gamePage.style.pointerEvents = "all"
+}
