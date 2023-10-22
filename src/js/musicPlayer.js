dd0b82c203310581b46f58beef3b8c235671e67c
dimitrod <dimtrod@googlemail.com>
2023-10-22 20:25:50 +0200

    Music Player minimized

--- src/js/musicPlayer.js
+++ src/js/musicPlayer.js
@@ -2,6 +2,10 @@ var myAudio = document.getElementById("music");
 var myAudioImage = document.getElementById("playpause");
 var isPlaying = false;
 var volume = document.getElementById('volume-slider');
+var musicWindowTrigger = document.getElementById('musicNote');
+var thePlayer = document.getElementById('musicPlayer');
+
+
 myAudio.volume = volume.value / 100;
 
 function togglePlay() {
@@ -20,3 +24,68 @@ myAudio.onpause = function() {
 volume.addEventListener("change", function(e) {
     myAudio.volume = e.currentTarget.value / 100;
 })
+
+musicWindowTrigger.addEventListener("click", function() {
+
+  if(this.className.match('is-active')) {
+
+    thePlayer.classList.toggle("is-active");
+
+    setTimeout(function(){ 
+  
+      thePlayer.classList.toggle("display-none");
+    }, 500); 
+
+  } else {
+
+      thePlayer.classList.toggle("display-none");
+ 
+
+    thePlayer.classList.toggle("is-active");
+  
+  }
+
+});
+
+musicWindowTrigger.addEventListener("mousedown", function() {
+  musicWindowTrigger.src="resource/music_note_pressed.png";
+});
+
+musicWindowTrigger.addEventListener("mouseup", function() {
+  musicWindowTrigger.src="resource/music_note.png";
+});
+
+
+
+
+myAudioImage.addEventListener("mousedown", function() {
+
+
+  if (isPlaying == false) {
+
+    myAudioImage.src="resource/play_pressed.png";
+
+  } else if (isPlaying == true) {
+
+    myAudioImage.src="resource/pause_pressed.png";
+
+  }
+
+});
+
+myAudioImage.addEventListener("mouseup", function() {
+
+  if (isPlaying == false) {
+
+    myAudioImage.src=="resource/play.png";
+
+  } else if (isPlaying == true) {
+
+    myAudioImage.src="resource/pause.png";
+
+  }
+
+});
+
+
+
